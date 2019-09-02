f9456aea0f9331326e93a3bbf69e80426e71f85b
Hridoy Mozumder <hrrarya6@gmail.com>
2019-09-02 13:47:08 +0600

    added some features(3rd)

--- js/main.js
+++ js/main.js
@@ -13,6 +13,7 @@ const submit = document.getElementById("submit");
 const { name } = list;
 let ulList;
 let clientList;
+let countClient;
 let del = document.querySelectorAll(".del");
 
 resetInput = () => {
@@ -21,24 +22,26 @@ resetInput = () => {
   checkOld.checked = false;
   checkEmergency.checked = false;
 };
-addName = name => {
+addName = singleName => {
   ulList = document.getElementById("ulList");
 
-  if (name.type == "old") {
-    ulList.innerHTML += `<li class="list-group-item old">${name.name} <span class="del" data-id="${name.id}">X</span><span class="del old">Old</span></li>`;
-  } else if (name.type == "emergency") {
-    ulList.innerHTML += `<li class="list-group-item emergency">${name.name} <span class="del" data-id="${name.id}">X</span><span class="del emergency">Emergency</span></li>`;
+  if (singleName.type == "old") {
+    ulList.innerHTML += `<li class="list-group-item old">${singleName.name} <span class="del" data-id="${singleName.id}">X</span><span class="del old">Old</span></li>`;
+  } else if (singleName.type == "emergency") {
+    ulList.innerHTML += `<li class="list-group-item emergency">${singleName.name} <span class="del" data-id="${singleName.id}">X</span><span class="del emergency">Emergency</span></li>`;
   } else {
-    ulList.innerHTML += `<li class="list-group-item">${name.name} <span class="del" data-id="${name.id}">X</span></li>`;
+    ulList.innerHTML += `<li class="list-group-item">${singleName.name} <span class="del" data-id="${singleName.id}">X</span></li>`;
   }
 
   del = document.querySelectorAll(".del");
 
   removeItem();
+  countClients(name.length);
 };
 
 function showList() {
   ulList = document.getElementById("ulList");
+
   name
     .filter(p => p.isRunning != true)
     .map(n => {
@@ -51,7 +54,7 @@ function showList() {
       }
     });
   del = document.querySelectorAll(".del");
-  // removeItem();
+  countClients(name.length);
 }
 
 handleSubmit = () => {
@@ -91,11 +94,22 @@ function removeItem() {
         .map(p => {
           p.isRunning = true;
           clientList.removeChild(targetName);
+          countClient = name.filter(n => n.isRunning !== true);
+          countClients(countClient.length);
         });
     });
   });
 }
+
+function countClients(totalClient) {
+  countClient = document.querySelector(".countClient");
+  if (totalClient > 0)
+    countClient.innerHTML = totalClient + " people is in the list";
+  else {
+    countClient.innerHTML = "No people in the list";
+  }
+}
+
 showList();
 removeItem();
 // showList(); //SHOW ALL NAME IN THE LIST;
-// console.log(ulList.childNodes);
