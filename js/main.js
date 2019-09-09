84a7473617a40167546b6ad89bc22612f9751c25
Hridoy Mozumder <hrrarya6@gmail.com>
2019-09-09 14:19:43 +0600

    solving nodeList not found(6th)

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 let list = {
-  name: []
+  name: [
+    { id: 1, name: "hridoy mozumder", type: "old", isRunning: false },
+    { id: 2, name: "Rimon mozumder", type: "old", isRunning: false },
+    { id: 3, name: "Rika mozumder", type: "emergency", isRunning: false }
+  ]
 };
-
 const input = document.getElementById("name");
 let checkOld = document.getElementById("checkOld");
 const submit = document.getElementById("submit");
@@ -11,6 +14,8 @@ let clientList;
 let countClient;
 let counter = 0;
 let del = document.querySelectorAll(".del");
+let emrg;
+let names = list.name;
 
 resetInput = () => {
   input.value = "";
@@ -20,18 +25,30 @@ resetInput = () => {
 };
 addName = singleName => {
   ulList = document.getElementById("ulList");
-  counter = name.length;
+  counter = names.length;
 
   if (singleName.type == "old") {
-    ulList.innerHTML += `<li class="list-group-item old">${singleName.name} <span class="del" data-id="${singleName.id}">X</span><span class="del old">Old</span></li>`;
+    ulList.innerHTML += `<li class="list-group-item old">${singleName.name} <span class="del" data-id="${singleName.id}">X</span><span class="delz old">Old</span></li>`;
   } else if (singleName.type == "emergency") {
-    ulList.innerHTML += `<li class="list-group-item emergency">${singleName.name} <span class="del" data-id="${singleName.id}">X</span><span class="del emergency">Emergency</span></li>`;
+    let newItem = document.createElement("li");
+    let spanDel = document.createElement("span");
+    let spanEmergency = document.createElement("span");
+    newItem.className = "list-group-item emergency";
+    newItem.innerHTML = singleName.name;
+    spanDel.className = "del";
+    spanDel.setAttribute("data-id", parseInt(singleName.id));
+    spanDel.innerHTML = "X";
+    spanEmergency.className = "delz emergency";
+    spanEmergency.innerHTML = "Emergency";
+
+    newItem.appendChild(spanDel);
+    newItem.appendChild(spanEmergency);
+    ulList.prepend(newItem);
   } else {
     ulList.innerHTML += `<li class="list-group-item">${singleName.name} <span class="del" data-id="${singleName.id}">X</span></li>`;
   }
 
   del = document.querySelectorAll(".del");
-
   removeItem();
   checkAvailableClients();
 };
@@ -39,13 +56,22 @@ addName = singleName => {
 function showList() {
   ulList = document.getElementById("ulList");
 
-  name
+  emrg = name.filter(n => n.type == "emergency");
+  emrg.map(n => {
+    let index = name.indexOf(n);
+    if (index > -1) {
+      name.splice(index, 1);
+    }
+  });
+  names = emrg.concat(name);
+
+  names
     .filter(p => p.isRunning != true)
     .map(n => {
       if (n.type == "old") {
-        ulList.innerHTML += `<li class="list-group-item old">${n.name} <span class="del" data-id="${n.id}">X</span> <span class="del old">Old</span></li>`;
+        ulList.innerHTML += `<li class="list-group-item old">${n.name} <span class="del" data-id="${n.id}">X</span> <span class="delz old">Old</span></li>`;
       } else if (n.type == "emergency") {
-        ulList.innerHTML += `<li class="list-group-item emergency">${n.name} <span class="del" data-id="${n.id}">X</span> <span class="del emergency">Emergency</span></li>`;
+        ulList.innerHTML += `<li class="list-group-item emergency">${n.name} <span class="del" data-id="${n.id}">X</span> <span class="delz emergency">Emergency</span></li>`;
       } else {
         ulList.innerHTML += `<li class="list-group-item">${n.name} <span class="del" data-id="${n.id}">X</span></li>`;
       }
@@ -59,7 +85,7 @@ handleSubmit = () => {
   let value = input.value;
   if (value != "") {
     let singleList = new Object();
-    singleList.id = name.length + 1;
+    singleList.id = names.length + 1;
     singleList.name = value;
     singleList.isRunning = false;
     if (checkOld.checked) {
@@ -69,7 +95,11 @@ handleSubmit = () => {
       singleList.type = "emergency";
     }
 
-    name.push(singleList);
+    if (singleList.type == "emergency") {
+      names.splice(0, 0, singleList);
+    } else {
+      names.push(singleList);
+    }
 
     addName(singleList);
   }
@@ -81,19 +111,21 @@ submit.addEventListener("click", e => {
   resetInput();
 });
 
-function removeItem() {
+function removeItem(clientList) {
   del.forEach(function(d) {
     d.addEventListener("click", function(e) {
+      let target = e.target;
+      console.log(target);
       let targetName = e.target.parentElement;
       clientList = document.getElementById("ulList");
-      let target = e.target;
-      name
+      names
         .filter(n => n.id == parseInt(target.attributes["data-id"].value))
         .map(p => {
           p.isRunning = true;
+          // countClient = names.filter(n => n.isRunning !== true);
           clientList.removeChild(targetName);
-          countClient = name.filter(n => n.isRunning !== true);
           checkAvailableClients();
+          // console.log(clientList.childNodes, targetName);
         });
     });
   });
@@ -102,19 +134,21 @@ function removeItem() {
 function countClients(totalClient) {
   countClient = document.querySelector(".countClient");
   if (totalClient > 0)
-    countClient.innerHTML = totalClient + " people is in the list";
+    countClient.innerHTML =
+      totalClient.toLocaleString("bn-BD") + " জন রোগী আছেন লিস্টে।";
   else {
     totalClient = 0;
-    countClient.innerHTML = "No people in the list";
+    countClient.innerHTML = "লিস্ট খালি!";
   }
 }
 
 function checkAvailableClients() {
-  let count = name.filter(n => n.isRunning !== true).length;
+  let count = names.filter(n => n.isRunning !== true).length;
   countClients(count);
 }
 
 showList();
 removeItem();
 
-// showList(); //SHOW ALL NAME IN THE LIST;
+// console.log(names);
+console.log(names);
